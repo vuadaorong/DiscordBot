d4f6e7be32b2767802d6c5ed5bfa30457ab171c7
vuadaorong <48785202+vuadaorong@users.noreply.github.com>
2020-08-28 22:45:08 +0700

    update ping

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     description: "This is a ping command!",
     execute(message, args){
-        message.channel.send('pong!');
+        message.channel.send('Tao là Bot, Ping con cặc!');
     }
 }
